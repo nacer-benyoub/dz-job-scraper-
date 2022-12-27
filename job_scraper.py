057ca86c52ff02cc3440a6234c9649845e957336
nacer-benyoub <nacerelveterinario@gmail.com>
2022-12-27 13:45:07 +0100

    add only unique new jobs to the csv/json file

--- job_scraper.py
+++ job_scraper.py
@@ -53,38 +53,67 @@ def scrape_jobs(soup):
                         })
     return jobs
 
+def combine_unique(df1: pd.DataFrame, df2: pd.DataFrame, *dfs: list[pd.DataFrame]) -> pd.DataFrame:
+    """Concatenate two or more dataframes keeping only unique records resetting the index
+
+    Args:
+        df1 (list[pandas.DataFrame]): First DataFrame
+        df2 (pandas.DataFrame): Second DataFrame
+        dfs (pandas.DataFrame): DataFrames to concatenate
+    
+    Returns:
+        pands.DataFrame: DataFrame containing concatenated dataframes
+    """        
+    in_dfs = [df1, df2] + list(dfs)
+    df = pd.concat(in_dfs, ignore_index=True)
+    return df.drop_duplicates(ignore_index=True)
+
 def jobs_to_csv(jobs, filename, mode='a'):
     """Save job data to a csv file
 
     Args:
-        jobs (_type_): list of jobs as dictionaries
-        filename (_type_): csv filename
+        jobs (list(dict)): list of jobs as dictionaries
+        filename (str): csv filename
         mode (str, optional): file access mode. Defaults to 'a+'.
     """    
-    df = pd.DataFrame(jobs)
-    print(df.head())
-    df.to_csv(filename, mode=mode, index=False)
+    new_jobs = pd.DataFrame(jobs)
+    if 'a' in mode and path.exists(filename):
+            existing_jobs = pd.read_csv(filename)
+            data = combine_unique(new_jobs, existing_jobs)
+            data.to_csv(filename, mode='w')
+            return
+    
+    new_jobs.to_csv(filename, mode=mode, index=False)
 
 def jobs_to_json(jobs, filename, mode='a+'):
     """Save job data to a json file
 
     Args:
-        jobs (_type_): list of jobs as dictionaries
-        filename (_type_): json filename
+        jobs (list(dict)): list of jobs as dictionaries
+        filename (str): json filename
         mode (str, optional): file access mode. Defaults to 'a+'.
     """    
-    if path.exists(filename):
-        with open(filename) as f:
-            existing_data = json.load(f)
-        
-        if 'a' in mode and len(existing_data):
-            with open(filename, "w") as f:
-                data = existing_data + jobs
-                json.dump(data, f, indent=1, allow_nan=True, ensure_ascii=False)
-            return
+    new_jobs = pd.DataFrame(jobs)
+    if 'a' in mode and path.exists(filename):
+            # with open(filename) as f:
+            #     existing_data = json.load(f)
             
-    with open(filename, "w") as f:
-        json.dump(jobs, f, indent=1, allow_nan=True, ensure_ascii=False)
+        
+        existing_jobs = pd.read_json(filename, encoding='utf-8')
+        print(existing_jobs.head())
+        data = combine_unique(new_jobs, existing_jobs)
+        data.to_json(filename, indent=1, force_ascii=False, orient="records")
+        return
+            
+        # with open(filename, "w") as f:
+        #     data = existing_data + jobs
+        #     json.dump(data, f, indent=1, allow_nan=True, ensure_ascii=False)
+        # return
+            
+    
+    new_jobs.to_json(filename, indent=1, force_ascii=False, orient="records")
+    # with open(filename, mode) as f:
+    #     json.dump(jobs, f, indent=1, allow_nan=True, ensure_ascii=False)
     
 if __name__=="__main__":
     soup = make_soup()
@@ -94,8 +123,8 @@ if __name__=="__main__":
     print(f"Saving job data to json file {json_filename}...")
     jobs_to_json(jobs, json_filename)
     print("Done", end="\n")
-    print(f"Saving job data to json file {csv_filename}...")
+    print(f"Saving job data to csv file {csv_filename}...")
     jobs_to_csv(jobs, csv_filename)
     print("Done")
     
-    # add logic to add only new jobs and avoid redundancy
+    # scrape more information about the jobs
